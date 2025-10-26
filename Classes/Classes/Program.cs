1f06de62458e8e15395650f1bbe4d71113999252
Kuyumcheva Sofia <kuyumcheva2001@gmail.com>
2025-10-27 00:47:38 +0300

    Homework4

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon knife = new Weapon("Нож");
+            knife.SetDamageParams(0, 0);
+            
+            var knifeDamage = knife.GetDamage();
+            Console.WriteLine("Урон: {0}", knifeDamage);
+
+            Weapon dagger = new Weapon("Кинжал", 3, 9);
+            var daggerDamage = dagger.GetDamage();
+            Console.WriteLine("Урон: {0}", daggerDamage);
+
+            Weapon sword = new Weapon("Меч", 10, 5);
+            var swordDamage = sword.GetDamage();
+            Console.WriteLine("Урон: {0}", swordDamage);
+
+        }
+    }
+}
---
+++ Classes/Classes/Weapon.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Classes
+{
+    internal class Weapon
+    {
+
+        public string Name { get; }
+
+        public int MinDamage { get; private set; }
+
+        public int MaxDamage { get; private set; }
+
+        public float Durability { get; }
+
+        public Weapon(string name)
+        {
+            Name = name;
+            Console.WriteLine(Name);
+            Durability = 1.0f;
+        }
+
+        public Weapon(string name, int minDamage, int maxDamage) : this(name)
+        {
+            SetDamageParams(minDamage, maxDamage);
+        }
+
+        public void SetDamageParams(int minDamage, int maxDamage)
+        {
+            int temp;
+
+            if(minDamage > maxDamage)
+            {
+                Console.WriteLine("Вы перепутали минимальный и максимальный урон");
+                temp = minDamage;
+                minDamage = maxDamage;
+                maxDamage = temp;
+            }
+
+            if(minDamage < 1)
+            {
+                Console.WriteLine("Указанное значение минимального урона слишком маленькое, мы автоматически установили его в 1");
+                minDamage = 1;
+            }
+
+            if(maxDamage <= 1)
+            {
+                Console.WriteLine("Указанное значение максимального урона слишком маленькое, мы автоматически установили его в 10");
+                maxDamage = 10;
+            }
+
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+
+        }
+
+        public int GetDamage()
+        {
+            return (MinDamage + MaxDamage)/2;
+        }
+
+    }
+}
